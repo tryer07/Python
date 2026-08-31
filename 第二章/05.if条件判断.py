a47fa10d1e6059cac2368f4a877a63605dc2ce2a
wen-yantong <1839632447@qq.com>
2026-08-31 14:14:39 +0800

    Python的学习内容

--- 第二章/05.if条件判断.py
+++ 第二章/05.if条件判断.py
@@ -53,15 +53,54 @@
 #     print('这个数是0')
 
 #练习4
-admin = '18806092052'
-password = 'wytlike688'
-admin1 = '1839632447'
-password1 = 'wytlike688'
-a = input('请输入账号：')
-b = input('请输入密码：')
-if a == '18806092052' and b == 'wytlike688':
-    print('登录成功！欢迎回来')
-elif a == '1839632447' and b == 'wytlike688':
-    print('登录成功！欢迎回来')
+# admin = '18806092052'
+# password = 'wytlike688'
+# admin1 = '1839632447'
+# password1 = 'wytlike688'
+# a = input('请输入账号：')
+# b = input('请输入密码：')
+# if a == '18806092052' and b == 'wytlike688':
+#     print('登录成功！欢迎回来')
+# elif a == '1839632447' and b == 'wytlike688':
+#     print('登录成功！欢迎回来')
+# else:
+#     print('登录失败(悲)')
+
+#练习5
+# a = int(input('请输入边长1：'))
+# b = int(input('请输入边长2：'))
+# c = int(input('请输入边长3：'))
+# if a + b > c and b + c > a and c + a > b:
+#     if a == b == c:
+#         print('这个三角形是等边三角形')
+#     elif a == b or b == c or a == c:
+#         print('这个三角形是等腰三角形')
+#     else:
+#         print('这是一个普通的三角形')
+# else:
+#     print('这三个边长不构成三角形哦')
+
+#练习6
+# day = int(input('请输入今天周几：'))
+# if day == 1 or day == 2 or day == 3 or day == 4 or day == 5:
+#     print('好好上班吧，牛马')
+# elif day == 6 or day == 7:
+#     print('劳资明天不上班')
+# else:
+#     print('输入错误！')
+
+day = int(input('请输入今天周几：'))
+if day == 1:
+    print('学习')
+elif day == 2:
+    print('出差')
+elif day == 3:
+    print('开会')
+elif day == 4:
+    print('项目报告')
+elif day == 5:
+    print('周总结')
+elif day == 6 or day == 7:
+    print('周末休息')
 else:
-    print('登录失败(悲)')
+    print('输入错误')
