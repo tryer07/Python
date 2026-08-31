86fda89504a1431646e228af07036b7983451c9c
wen-yantong <1839632447@qq.com>
2026-08-31 15:03:14 +0800

    Python的学习内容

---
+++ 第二章/07.while循环.py
@@ -0,0 +1,18 @@
+#while循环结构
+
+#while循环的基本结构
+i = 0
+while i < 10:
+    print('我还要向天再借五百年~')
+    i += 1
+else:
+    print('信用不足，无法借用')
+
+#计算1-100之间偶数的和
+total = 0
+i = 1
+while i < 101:
+    if i % 2 == 0:
+        total = total + i
+    i = i + 1
+    print(f'1-100之间偶数的是：{total}')
