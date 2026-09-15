992a84a9d270b4af3b82e38b5dc14bbf01173ada
外收内放 <1839632447@qq.com>
2026-09-15 19:08:27 +0800

    Python的学习内容

---
+++ 05_第五章（模块）/01.导入软件包中的模块.py
@@ -0,0 +1,15 @@
+#导入模块
+
+# import utils.my_function
+#
+# utils.my_function.log_separator1()
+# utils.my_function.log_separator2()
+# utils.my_function.log_separator3()
+# utils.my_function.log_separator4()
+
+from utils import my_function
+
+my_function.log_separator1()
+my_function.log_separator2()
+my_function.log_separator3()
+my_function.log_separator4()
---
+++ 05_第五章（模块）/utils/my_function.py
@@ -0,0 +1,13 @@
+#函数
+
+def log_separator1():
+    print('-' * 10)
+
+def log_separator2():
+    print('+' * 10)
+
+def log_separator3():
+    print('-' * 10)
+
+def log_separator4():
+    print('*' * 10)
