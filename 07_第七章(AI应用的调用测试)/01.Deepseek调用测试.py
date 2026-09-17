41de61f6de946f2471262badd82545c9a1b15fd5
外收内放 <1839632447@qq.com>
2026-09-17 18:16:59 +0800

    Python的学习内容

---
+++ 07_第七章(AI应用的调用测试)/01.Deepseek调用测试.py
@@ -0,0 +1,26 @@
+import os
+from openai import OpenAI
+
+#创建与AI交互的大模型客户端对象(DEEPSEEK_API_KEY环境变量的名字，值就是DeepSeek的API_KEY的)
+
+client = OpenAI(
+    api_key=os.environ.get('DEEPSEEK_API_KEY'),
+    base_url="https://api.deepseek.com")
+
+#与AI大模型进行交互()
+
+# noinspection bad-argument-type
+response = client.chat.completions.create(
+    model="deepseek-flash",
+    messages=[
+        {"role": "system", "content": "你是一名通识专家，通晓并精通所有的知识，会以严谨的逻辑和亲切的语气回答我的问题并给予充分的情绪价值"},
+        {"role": "user", "content": "你是谁？你能帮我做什么"},
+    ],
+    stream=False,
+    reasoning_effort="high",
+    extra_body={"thinking": {"type": "enabled"}}
+)
+
+#输出大模型返回的结果
+
+print(response.choices[0].message.content)
