e3e69be75e7e42cce38b461f7a14873538af8918
外收内放 <1839632447@qq.com>
2026-09-20 20:09:07 +0800

    Python的学习内容

--- 08_第八章（AI应用实战）/01.文件操作入门（文件的读和写）.py
+++ 08_第八章（AI应用实战）/01.文件操作入门（文件的读和写）.py
@@ -2,7 +2,7 @@
 
 #1.打开文件
 
-# f = open('resources/Python文件调用测试.txt', 'r', encoding ='utf-8')
+#  f = open('resources/Python文件调用测试.txt', 'r', encoding ='utf-8')
 
 #2.读取文件内容
 
@@ -17,15 +17,15 @@
 
 # f.close()
 
-#-------------------------------------------------------------------
+# -------------------------------------------------------------------
 
-#写文件
+# 写文件
 
-#1.创建文件
+# 1.创建文件
 
-f = open('resources/静夜思.txt', 'w', encoding ='utf-8')
+f = open('resources/静夜思.txt', 'w', encoding='utf-8')
 
-#2.写入文件内容
+# 2.写入文件内容
 
 f.write('静夜思\n')
 f.write('床前明月光\n')
@@ -33,6 +33,6 @@ f.write('疑是地上霜\n')
 f.write('举头望明月\n')
 f.write('低头思故乡\n')
 
-#3.关闭文件
+# 3.关闭文件
 
 f.close()
