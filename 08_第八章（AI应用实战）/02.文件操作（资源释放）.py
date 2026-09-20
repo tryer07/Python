b4be81492247fb1edb6a073553c32673f80ccfc3
外收内放 <1839632447@qq.com>
2026-09-20 20:12:54 +0800

    Python的学习内容

--- 08_第八章（AI应用实战）/02.文件操作（资源释放）.py
+++ 08_第八章（AI应用实战）/02.文件操作（资源释放）.py
@@ -1,15 +1,21 @@
-#下面的写法保证了当文件操作完成后文件一定会被关闭，即使在操作过程中发生异常文件也会被关闭，推荐使用try...finally结构。
+#下面的写法保证了当文件操作完成后文件一定会被关闭，即使在操作过程中发生异常文件也会被关闭。
+
+#方法一：使用try...finally结构(繁琐)
 
 #1.打开文件
 
 f = open('resources/资源释放', 'w', encoding ='utf-8')
 
 try:
-    #写入文件内容
+    #2.写入文件内容
     f.write('Hello, World!')
 
 
 finally:
-    #关闭文件
+    #3.关闭文件
     f.close()
     print('文件已关闭')
+
+# 方式二：使用with语句(更加推荐使用，因为它更简洁，且能自动处理文件的关闭。)
+with open('resources/资源释放', 'w', encoding ='utf-8') as f:
+    f.write('Hello, World!')
